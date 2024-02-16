d7e781e022b5775b36815c1da43eeb8f0ded8ae7
Ivan Pavlenko <ivan_pavlenko@knu.ua>
2024-02-16 11:08:31 +0200

    added main,py and triangle.py

---
+++ triangle.py
@@ -0,0 +1,23 @@
+class Triangle:
+
+    def __init__(self, side_a, side_b, side_c):
+        list_of_sides = [side_a, side_b, side_c]
+
+        list_of_sides.sort()
+
+        a, b, c = list_of_sides
+
+        assert c < a + b
+
+        self.a = side_a
+        self.b = side_b
+        self.c = side_c
+
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+    def square(self):
+        p = self.perimeter() / 2
+
+        return (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
