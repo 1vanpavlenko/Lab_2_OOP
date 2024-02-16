ce63df2c6b298dac06117c3a37f92c0b891aaec3
John Doe <snikita2206@gmail.com>
2024-02-16 11:29:37 +0200

    modding_Trapaze

--- Trapaze.py
+++ Trapaze.py
@@ -1,5 +1,14 @@
 class Trapezoid:
-    def __init__(self, a, b, c, d):
+    def __init__(self, side_a, side_b, side_c, side_d):
+
+        list_of_sides = [side_a, side_b, side_c, side_d]
+
+        list_of_sides.sort()
+
+        a, b, c, d = list_of_sides 
+
+        assert a > 0 and b > 0 and c > 0 and d > 0
+
         self.a = a
         self.b = b
         self.c = c
